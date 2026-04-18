6711927c47676e16f2e7d49087036a9f5e2bb460
prassannakumar-eng <prassannakumarsp@gmail.com>
2026-04-18 10:19:42 +0530

    Create decision_tree.py

---
+++ decision_tree.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+data = load_iris()
+X = data.data
+y = data.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+plt.figure(figsize=(12,8))
+plot_tree(model, feature_names=data.feature_names, class_names=data.target_names, filled=True)
+plt.show()
